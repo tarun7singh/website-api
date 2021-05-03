9f99d6ac3f0da59b00b6635beb0bb9bb2854b4b9
tarun7singh <taruninsa1@gmail.com>
2021-05-03 18:51:38 +0530

    body parser logic

--- server.ts
+++ server.ts
@@ -1,11 +1,19 @@
 require("dotenv").config();
 import express from "express";
+
 const app = express();
+
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: false,
+  })
+);
+
 const port = process.env.PORT || 5000;
 import * as imageController from "./src/controllers/image.controller";
 // import * as pnpController from "./controllers/pnp.controller";
 // import * as crsController from "./controllers/crs.controller";
-
 
 // app.get("/pnp-service", pnpController.pnp);
 // app.get("/crs-service", crsController.crs);
@@ -21,5 +29,10 @@ app.post("/image-generator/prepare", imageController.prepare);
 // app.get("/image-generator/approve/:name", imageController.approveStoredImage);
 // app.get("/image-generator/deny/:name", imageController.deleteStoredImage);
 
-app.listen(5000, () => console.log(`Server started on : http://localhost:${port}`));
-process.on('SIGINT', () => { console.log("Bye bye!"); process.exit(); })
+app.listen(5000, () =>
+  console.log(`Server started on : http://localhost:${port}`)
+);
+process.on("SIGINT", () => {
+  console.log("Bye bye!");
+  process.exit();
+});
